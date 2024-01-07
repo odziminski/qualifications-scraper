aa37456f642d50ac191932f339824987acf66baa
odziminski <o.dziminski@gmail.com>
2024-01-07 21:20:01 +0100

    get skills and its level required, write it into the array

--- scraper.php
+++ scraper.php
@@ -48,8 +48,30 @@ for ($i = 0; $i <= 20; $i++) {
 }
 
 $uniqueHrefsArray = array_unique($hrefsArray);
-
-var_dump($uniqueHrefsArray);
 var_dump(count($uniqueHrefsArray));
+$qualificationsArray = [];
+foreach ($uniqueHrefsArray as $href) {
+    $driver->get('https://justjoin.it' . $href);
+    sleep(2);
+    $i = 1;
+    while (true) {
+        $xpathSkill = "/html/body/div[1]/div[2]/div[2]/div/div[2]/div[2]/div[3]/div/ul/div[$i]/div/h6";
+        $xpathLevel = "/html/body/div[1]/div[2]/div[2]/div/div[2]/div[2]/div[3]/div/ul/div[$i]/div/span";
 
+        $elementsSkill = $driver->findElements(WebDriverBy::xpath($xpathSkill));
+        $elementsLevel = $driver->findElements(WebDriverBy::xpath($xpathLevel));
 
+        if (count($elementsSkill) == 0) {
+            break;
+        }
+        foreach ($elementsSkill as $elementSkill) {
+            $skill = $elementSkill->getText();
+        }
+        foreach ($elementsLevel as $elementLevel) {
+            $level = $elementLevel->getText();
+        }
+        $qualificationsArray[$skill] = $level;
+        $i++;
+    }
+}
+var_dump($qualificationsArray);
