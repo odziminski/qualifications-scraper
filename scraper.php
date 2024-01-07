649e7a03c5746d590b5c5226d18fb9d65638bc71
odziminski <o.dziminski@gmail.com>
2024-01-07 20:05:15 +0100

    get all links from the page using hardcoded bodyheight divided by 20

--- scraper.php
+++ scraper.php
@@ -17,3 +17,39 @@ $capabilities = DesiredCapabilities::chrome();
 $capabilities->setCapability('chrome.binary', $chromeBinaryPath);
 
 $driver = RemoteWebDriver::create($host, $capabilities);
+
+$driver->get('https://justjoin.it/all-locations/php');
+
+$bodyHeight = $driver->executeScript("return document.body.scrollHeight;");
+$hrefsArray = [];
+$previousNumberOfLinks = 0;
+$currentNumberOfLinks = 0;
+
+$scrollStep = $bodyHeight / 20;
+
+for ($i = 0; $i <= 20; $i++) {
+    $previousNumberOfLinks = count($driver->findElements(WebDriverBy::tagName('a')));
+
+    $driver->executeScript("window.scrollTo(0, $scrollStep * $i);");
+
+    sleep(1);
+
+    $currentNumberOfLinks = count($driver->findElements(WebDriverBy::tagName('a')));
+
+    $links = $driver->findElements(WebDriverBy::tagName('a'));
+
+    foreach ($links as $link) {
+        $href = $link->getAttribute('href');
+
+        if (str_starts_with(strval($href), '/offers/')) {
+            $hrefsArray[] = $href;
+        }
+    }
+}
+
+$uniqueHrefsArray = array_unique($hrefsArray);
+
+var_dump($uniqueHrefsArray);
+var_dump(count($uniqueHrefsArray));
+
+
