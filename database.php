4253a042da665ce0b621896e2fbd5a46d52fc2d9
odziminski <o.dziminski@gmail.com>
2024-02-08 21:48:11 +0100

    fetch information from DB using ajax when clicking on category a href

--- database.php
+++ database.php
@@ -3,18 +3,17 @@
 class DB
 {
     private static $instance = NULL;
-    
+
     public static function connect(): ?PDO
     {
         $host = $_ENV['DB_HOST'];
         $db = $_ENV['DB_DATABASE'];
         $user = $_ENV['DB_USER'];
 
+        $dbh = new PDO("mysql:host=$host;dbname=$db", $user);
+        self::$instance = $dbh;
 
-            $dbh = new PDO("mysql:host=$host;dbname=$db", $user);
-            self::$instance = $dbh;
-
-            return self::$instance;
+        return self::$instance;
     }
 
     public static function insert($category, $skills, $offers_count): void
@@ -22,8 +21,17 @@ class DB
         $pdo = self::connect();
         $sql = "INSERT INTO qualifications (category,skills, offers_count) VALUES (?,?,?)";
         $query = $pdo->prepare($sql);
-        if ($query->execute([$category, $skills, $offers_count])){
+        if ($query->execute([$category, $skills, $offers_count])) {
             echo "New record created successfully \n";
         }
+    }
+
+    public static function getByCategory($category): array
+    {
+        $pdo = self::connect();
+        $sql = "SELECT * FROM qualifications WHERE category = ?";
+        $query = $pdo->prepare($sql);
+        $query->execute([$category]);
+        return $query->fetchAll(PDO::FETCH_ASSOC);
     }
 }
