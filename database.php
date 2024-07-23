e0450f9cb2531c7e9262f2ab6b1ae69cef2f3513
odziminski <o.dziminski@gmail.com>
2024-07-23 21:13:28 +0200

    rename getByCategory to getTechByCategory

--- database.php
+++ database.php
@@ -26,7 +26,7 @@ class DB
         }
     }
 
-    public static function getByCategory($category): array | bool
+    public static function getTechByCategory($category): array | bool
     {
         $pdo = self::connect();
         $sql = "SELECT id,category, skills, offers_count, added_at
